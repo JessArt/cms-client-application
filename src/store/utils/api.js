eea65344d683c59570b2dd70c0fe6a2b34edebf2
Seva Zaikov <seva.zaikov@gmail.com>
2017-12-24 18:57:10 +0100

    switch to the new API

--- src/store/utils/api.js
+++ src/store/utils/api.js
@@ -1,36 +1,39 @@
-import { stringify } from 'query-string';
+import { stringify } from "query-string";
+import { store, actions } from "../index";
 
-const prefix = 'https://cms.jess.gallery';
+const prefix = "https://node-api.jess.gallery";
 
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
+  } else if (response.status === 401) {
+    store.dispatch(actions.auth.status(false));
+    localStorage.removeItem("token");
+  } else {
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
   }
-
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
 }
 
 function parseJSON(response) {
   return response.json();
 }
 
-
 function processURL(url, parameters) {
   const urlWithPrefix = `${prefix}${url}`;
-  const stringifiedParams = parameters ? `?${stringify(parameters)}` : '';
+  const stringifiedParams = parameters ? `?${stringify(parameters)}` : "";
   return urlWithPrefix + stringifiedParams;
 }
 
 export function get(url, parameters) {
   const processedURL = processURL(url, parameters);
   return fetch(processedURL, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      Accept: 'application/json',
-      Authorization: 'Some test request',
-    },
+      Accept: "application/json",
+      Authorization: "Some test request"
+    }
   })
     .then(checkStatus)
     .then(parseJSON);
@@ -39,14 +42,27 @@ export function get(url, parameters) {
 export function post(url, form) {
   const processedURL = processURL(url);
   return fetch(processedURL, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      Accept: 'application/json',
-      Authorization: 'Some test request',
+      Accept: "application/json",
+      Authorization: "Some test request"
     },
-    body: form,
+    body: form
   })
     .then(checkStatus)
     .then(parseJSON);
 }
 
+export function put(url, form) {
+  const processedURL = processURL(url);
+  return fetch(processedURL, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      Authorization: "Some test request"
+    },
+    body: form
+  })
+    .then(checkStatus)
+    .then(parseJSON);
+}
