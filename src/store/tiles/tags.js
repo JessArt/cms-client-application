dc93ae309cf7fd047866f9e301a436ff46460aa3
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-04 08:19:20 +0200

    add top tags and tags updating to store tile

--- src/store/tiles/tags.js
+++ src/store/tiles/tags.js
@@ -2,8 +2,39 @@ import { createTile } from "redux-tiles";
 
 export const tags = createTile({
   type: ["api", "tags"],
-  fn: ({ api }) => api.get("/v1/tags").then(({ data }) => data),
+  fn: ({ api, params }) => api.get("/v1/tags", params).then(({ data }) => data),
   caching: true
+});
+
+export const topTags = createTile({
+  type: ["api", "topTags"],
+  fn: ({ api, params }) =>
+    api
+      .get("/v1/top_tags", params)
+      .then(data => data.map(({ tag_id }) => tag_id)),
+  nesting: ({ type }) => [type]
+});
+
+export const updateTopTags = createTile({
+  type: ["api", "updateTopTags"],
+  fn: ({ api, params, selectors, getState }) => {
+    const token = selectors.auth.status(getState());
+    return api.post(
+      `/v1/top_tags?token=${token}&type=${params.type}`,
+      params.form
+    );
+  },
+  nesting: ({ type }) => [type]
+});
+
+export const updateTag = createTile({
+  type: ["api", "updateTag"],
+  fn: ({ api, params, selectors, getState }) => {
+    const token = selectors.auth.status(getState());
+
+    return api.put(`/v1/tags/${params.id}?token=${token}`, params.form);
+  },
+  nesting: ({ id }) => [id]
 });
 
 export const imageTags = createTile({
@@ -12,4 +43,4 @@ export const imageTags = createTile({
   nesting: ({ id }) => [id]
 });
 
-export default [tags, imageTags];
+export default [tags, imageTags, updateTag, topTags, updateTopTags];
