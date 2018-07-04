fed3e07a29938c59c406f3f1ed1aedef6e4ca681
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-04 08:25:34 +0200

    add __LOCAL__ variable to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
+
+console.log("BUIDLING", isProduction);
 
 module.exports = {
   entry: isProduction
-    ? [
-      'babel-polyfill',
-      './src/index.js',
-    ]
+    ? ["babel-polyfill", "./src/index.js"]
     : [
-      'react-hot-loader/patch',
-      // activate HMR for React
+        "react-hot-loader/patch",
+        // activate HMR for React
 
-      'webpack-dev-server/client?http://localhost:3000',
-      // bundle the client for webpack-dev-server
-      // and connect to the provided endpoint
+        "webpack-dev-server/client?http://localhost:3000",
+        // bundle the client for webpack-dev-server
+        // and connect to the provided endpoint
 
-      'webpack/hot/only-dev-server',
-      // bundle the client for hot reloading
-      // only- means to only hot reload for successful updates
+        "webpack/hot/only-dev-server",
+        // bundle the client for hot reloading
+        // only- means to only hot reload for successful updates
 
-      'babel-polyfill',
-      './src/index.js',
-    ],
-  devtool: isProduction ? 'cheap-source-map' : 'inline-source-map',
+        "babel-polyfill",
+        "./src/index.js"
+      ],
+  devtool: isProduction ? "cheap-source-map" : "inline-source-map",
   output: {
-    filename: 'build.js',
-    path: path.join(__dirname, 'build'),
-    publicPath: '/',
+    filename: "build.js",
+    path: path.join(__dirname, "build"),
+    publicPath: "/"
   },
   module: {
     rules: [
@@ -36,57 +35,62 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
-        },
+          loader: "babel-loader"
+        }
       },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader',
+        loader: "style-loader!css-loader"
       },
       {
         test: /\.sass$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader?indentedSyntax',
-      },
-    ],
+        loader:
+          "style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader?indentedSyntax"
+      }
+    ]
   },
   plugins: isProduction
     ? [
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: '"production"',
-        },
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        beautify: false,
-        mangle: {
-          screw_ie8: true,
-          keep_fnames: true,
-        },
-        warnings: false,
-        compress: {
-          screw_ie8: true,
-        },
-        comments: false,
-      }),
-    ]
+        new webpack.DefinePlugin({
+          __LOCAL__: JSON.stringify(!isProduction),
+          "process.env": {
+            NODE_ENV: '"production"'
+          }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+          beautify: false,
+          mangle: {
+            screw_ie8: true,
+            keep_fnames: true
+          },
+          warnings: false,
+          compress: {
+            screw_ie8: true
+          },
+          comments: false
+        })
+      ]
     : [
-      new webpack.HotModuleReplacementPlugin(),
-      // enable HMR globally
+        new webpack.DefinePlugin({
+          __LOCAL__: JSON.stringify(!isProduction)
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        // enable HMR globally
 
-      new webpack.NamedModulesPlugin(),
-      // prints more readable module names in the browser console on HMR updates
-    ],
+        new webpack.NamedModulesPlugin()
+        // prints more readable module names in the browser console on HMR updates
+      ],
 
   devServer: {
-    host: 'localhost',
+    host: "localhost",
     port: 3000,
-    contentBase: path.join(__dirname, 'build'),
-    publicPath: '/',
+    contentBase: path.join(__dirname, "build"),
+    publicPath: "/",
 
     historyApiFallback: true,
     // respond to 404s with index.html
 
-    hot: true,
+    hot: true
     // enable HMR on the server
-  },
+  }
 };
