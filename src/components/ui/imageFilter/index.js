7710e543e10e9d4f5dacd2893acd69eb6adb884a
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-04 08:21:41 +0200

    add image filter component

--- src/components/ui/imageFilter/index.js
+++ src/components/ui/imageFilter/index.js
@@ -1,42 +1,120 @@
-import React from 'react';
-import RPT from 'prop-types';
-import { Link, withRouter } from 'react-router-dom';
-import { parse } from 'query-string';
-import { processURL } from '../../../utils/urls';
-import { DEFAULT_IMAGE_TYPE } from '../../../utils/constants';
-import styles from './style.sass';
+import React, { Component } from "react";
+import RPT from "prop-types";
 
-const constructLink = ({ pathname, type }) => processURL(pathname, {
-  type,
-  page: 0,
+import { connect } from "react-redux";
+import { actions, selectors } from "../../../store";
+import { Link, withRouter } from "react-router-dom";
+
+import ComplexSelect from "../../elements/complexSelect";
+
+import { parse } from "query-string";
+import { processURL } from "../../../utils/urls";
+import { DEFAULT_IMAGE_TYPE } from "../../../utils/constants";
+
+import styles from "./style.sass";
+
+const constructLink = ({ pathname, type, tags }) =>
+  processURL(pathname, {
+    type,
+    tags,
+    page: 0
+  });
+
+const mapStateToProps = state => ({
+  tags: selectors.api.tags(state)
 });
 
-const ImageFilter = ({ location: { pathname, search } }) => {
-  const currentParams = parse(search);
-  const activeSection = currentParams.type || DEFAULT_IMAGE_TYPE;
-  const artLink = constructLink({ pathname, type: 'art' });
-  const photoLink = constructLink({ pathname, type: 'photo' });
-  const otherLink = constructLink({ pathname, type: 'other' });
-  return (
-    <div className={styles.container}>
-      <Link className={styles.elem} to={artLink}>
-        {'Art'}
-      </Link>
-      <Link className={styles.elem} to={photoLink}>
-        {'Photo'}
-      </Link>
-      <Link className={styles.elem} to={otherLink}>
-        {'Other'}
-      </Link>
-    </div>
-  );
+const mapDispatchToProps = {
+  fetchTags: actions.api.tags
 };
 
-ImageFilter.propTypes = {
-  location: RPT.shape({
-    pathname: RPT.string,
-    search: RPT.string,
-  }),
-};
+class ImageFilter extends Component {
+  static propTypes = {
+    location: RPT.shape({
+      pathname: RPT.string,
+      search: RPT.string
+    }),
+    fetchTags: RPT.func,
+    tags: RPT.object
+  };
 
-export default withRouter(ImageFilter);
+  componentDidMount() {
+    this.props.fetchTags();
+  }
+
+  handleTags = tags => {
+    const { location: { pathname, search }, history } = this.props;
+    const currentParams = parse(search);
+    const newLink = constructLink({
+      pathname,
+      type: currentParams.type,
+      tags: tags.map(({ value }) => value)
+    });
+
+    history.push(newLink);
+  };
+
+  renderTags() {
+    const { tags, location: { search } } = this.props;
+
+    const currentParams = parse(search);
+
+    if (tags.isPending || !tags.data) {
+      return;
+    }
+
+    const tagOptions = (tags.data || []).map(tag => ({
+      value: tag.id,
+      label: tag.name.toLowerCase()
+    }));
+
+    let value = [];
+    if (currentParams.tags) {
+      value = Array.isArray(currentParams.tags)
+        ? currentParams.tags
+        : [currentParams.tags];
+    }
+
+    return (
+      <div className={styles.tags}>
+        <ComplexSelect
+          onChange={this.handleTags}
+          multiple
+          name={"tags"}
+          options={tagOptions}
+          value={value}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    const { location: { pathname, search }, data } = this.props;
+
+    const currentParams = parse(search);
+    const activeSection = currentParams.type || DEFAULT_IMAGE_TYPE;
+    const artLink = constructLink({ pathname, type: "art" });
+    const photoLink = constructLink({ pathname, type: "photo" });
+    const otherLink = constructLink({ pathname, type: "other" });
+    return (
+      <div>
+        <div className={styles.container}>
+          <Link className={styles.elem} to={artLink}>
+            {"Art"}
+          </Link>
+          <Link className={styles.elem} to={photoLink}>
+            {"Photo"}
+          </Link>
+          <Link className={styles.elem} to={otherLink}>
+            {"Other"}
+          </Link>
+        </div>
+        {this.renderTags()}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withRouter(ImageFilter)
+);
