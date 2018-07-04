816398a39deb1dc5cf86090b15a5df0b8505932b
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-04 08:22:01 +0200

    allow to pass your own content into ErrorContainer

--- src/components/ui/errorContainer/index.js
+++ src/components/ui/errorContainer/index.js
@@ -6,16 +6,18 @@ import Button from "../../elements/button";
 
 export default class ErrorContainer extends React.Component {
   static propTypes = {
-    refresh: RPT.func
+    refresh: RPT.func,
+    children: RPT.node
   };
 
   render() {
-    const { refresh } = this.props;
+    const { refresh, children } = this.props;
     return (
       <div className={styles.container}>
         <h1 className={styles.title}>
           {"Sorry, something went wrong. Please, try again."}
         </h1>
+        {children && <div className={styles.content}>{children}</div>}
         {refresh && (
           <Button className={styles.button} onClick={refresh}>
             {"Try to load again"}
