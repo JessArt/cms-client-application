e43749df96ea7961fcdc6cfd855f1691dfd6f360
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-04 08:25:08 +0200

    update code formatting in layout component

--- src/components/layouts/application/index.js
+++ src/components/layouts/application/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import RPT from 'prop-types';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { selectors } from '../../../store';
-import Menu from '../../ui/menu';
-import Notifications from '../../ui/notifications';
-import styles from './style.sass';
-import ScrollTop from '../../ui/scrollTop';
+import React from "react";
+import RPT from "prop-types";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { selectors } from "../../../store";
+import Menu from "../../ui/menu";
+import Notifications from "../../ui/notifications";
+import styles from "./style.sass";
 
 const mapStateToProps = state => ({
-  isAuthorized: selectors.auth.status(state),
+  isAuthorized: selectors.auth.status(state)
 });
 
 const ApplicationLayout = ({ isAuthorized, children }) => (
   <div className={styles.container}>
-    <ScrollTop />
     <Notifications />
-    {isAuthorized &&
+    {isAuthorized && (
       <div>
         <div className={`${styles.menu} ${styles.invisible}`}>
           <Menu />
@@ -25,16 +23,14 @@ const ApplicationLayout = ({ isAuthorized, children }) => (
           <Menu />
         </div>
       </div>
-    }
-    <div className={styles.content}>
-      {children}
-    </div>
+    )}
+    <div className={styles.content}>{children}</div>
   </div>
 );
 
 ApplicationLayout.propTypes = {
   children: RPT.node,
-  isAuthorized: RPT.bool,
+  isAuthorized: RPT.bool
 };
 
 export default withRouter(connect(mapStateToProps)(ApplicationLayout));
