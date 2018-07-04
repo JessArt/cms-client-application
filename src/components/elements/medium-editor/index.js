b3a85a9da5990786e021f54c3b66d2f2dc2e58cb
Seva Zaikov <seva.zaikov@gmail.com>
2018-07-04 08:24:49 +0200

    update medium editor component

--- src/components/elements/medium-editor/index.js
+++ src/components/elements/medium-editor/index.js
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
-import MediumEditor from 'medium-editor';
-import 'medium-editor/dist/css/medium-editor.css';
-import 'medium-editor/dist/css/themes/default.css';
-import { connect } from 'react-redux';
-import { actions } from '../../../store';
-import { readFile } from '../../../utils/files';
-import styles from './style.sass';
+import React, { Component } from "react";
+import MediumEditor from "medium-editor";
+import "medium-editor/dist/css/medium-editor.css";
+import "medium-editor/dist/css/themes/default.css";
+import { connect } from "react-redux";
+import { actions } from "../../../store";
+import { readFile } from "../../../utils/files";
+import styles from "./style.sass";
+import progressStyles from "./progress.sass";
 
 const mapDispatchToProps = {
-  uploadPicture: actions.api.uploadPicture,
+  uploadPicture: actions.api.uploadPicture
 };
 
 function insertImage({ e, url }) {
-  const img = document.createElement('img');
-  img.setAttribute('src', url);
+  const img = document.createElement("img");
+  img.setAttribute("src", url);
   e.target.parentNode.insertBefore(img, e.target.nextElementSibling);
 }
 
+function createProgressBar() {
+  const container = document.createElement("div");
+  container.classList.add(progressStyles.meter, progressStyles.animate);
+
+  const innerSpan = document.createElement("span");
+  innerSpan.style.width = "100%";
+  const stripes = document.createElement("span");
+
+  innerSpan.appendChild(stripes);
+  container.appendChild(innerSpan);
+
+  return container;
+}
+
 function loadImage(url) {
-  return new Promise((res) => {
+  return new Promise(res => {
     const img = new Image();
     img.onload = () => res(img);
     img.src = url;
@@ -29,13 +44,13 @@ async function resizeInCanvas(imgURL) {
   const img = await loadImage(imgURL);
   const perferedWidth = 2400;
   const ratio = perferedWidth / img.width;
-  const canvas = document.createElement('canvas');
+  const canvas = document.createElement("canvas");
   canvas.width = img.width * ratio;
   canvas.height = img.height * ratio;
-  const ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext("2d");
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-  return new Promise((res) => {
-    canvas.toBlob(blob => res(blob), 'image/jpeg', 1);
+  return new Promise(res => {
+    canvas.toBlob(blob => res(blob), "image/jpeg", 1);
   });
 }
 
@@ -44,50 +59,143 @@ class Editor extends Component {
     const { uploadPicture } = this.props;
     const editor = new MediumEditor(this._container, {
       paste: {
-        forcePlainText: false,
+        forcePlainText: false
       },
       toolbar: {
         buttons: [
-          'bold', 'italic', 'underline',
-          'h1', 'h2', 'h3',
-          'image',
-          'justifyLeft', 'justifyCenter', 'quote',
-        ],
-      },
+          "bold",
+          "italic",
+          "underline",
+          "anchor",
+          "h1",
+          "h2",
+          "h3",
+          "image",
+          "justifyLeft",
+          "justifyCenter",
+          "quote"
+        ]
+      }
     });
 
-    editor.subscribe('editablePaste', async (e) => {
-      const items = (event.clipboardData || event.originalEvent.clipboardData).items;
+    editor.subscribe("editablePaste", async e => {
+      const items = (event.clipboardData || event.originalEvent.clipboardData)
+        .items;
+
+      const filesArray = [];
+
       for (const index in items) {
         const item = items[index];
-        if (item.kind === 'file') {
-          const blob = item.getAsFile();
-          const { url: originalImageUrl } = await readFile(blob);
+        if (item.kind === "file") {
+          filesArray.push(item.getAsFile());
+        }
+      }
+
+      filesArray.reduce(
+        (promise, blob) =>
+          promise.then(async () => {
+            const { url: originalImageUrl } = await readFile(blob);
+            const smallFile = await resizeInCanvas(originalImageUrl);
+
+            const form = new FormData();
+            form.append("image", smallFile);
+
+            const progress = createProgressBar();
+            e.target.parentNode.insertBefore(
+              progress,
+              e.target.nextElementSibling
+            );
+
+            uploadPicture({
+              form,
+              cb: ({ url }) => {
+                const img = document.createElement("img");
+                img.setAttribute("src", url);
+                progress.parentElement.replaceChild(img, progress);
+              }
+            });
+          }),
+        Promise.resolve()
+      );
+    });
+
+    editor.subscribe("editableDrop", async function(event) {
+      const srcElement = event.srcElement;
+
+      var dt = event.dataTransfer;
+      var items = dt.items;
+
+      const filesArray = [];
+
+      for (const index in items) {
+        const item = items[index];
+        if (item.kind === "file") {
+          filesArray.push(item.getAsFile());
+        }
+      }
+
+      const images = await Promise.all(
+        filesArray.map(blob =>
+          readFile(blob).then(({ url: originalImageUrl }) => {
+            const img = document.querySelectorAll(
+              `[src="${originalImageUrl}"]`
+            );
+
+            const imgElement = img && img[0];
+            if (imgElement) {
+              imgElement.style.display = "none";
+            }
+
+            return { originalImageUrl, imgElement };
+          })
+        )
+      );
+
+      images.reduce((promise, { originalImageUrl, imgElement }) => {
+        return promise.then(async () => {
+          const progress = createProgressBar();
+
+          if (imgElement) {
+            imgElement.parentElement.insertBefore(progress, imgElement);
+            imgElement.style.display = "none";
+          }
+
           const smallFile = await resizeInCanvas(originalImageUrl);
 
           const form = new FormData();
-          form.append('image', smallFile);
+          form.append("image", smallFile);
 
-          uploadPicture({
+          await uploadPicture({
             form,
-            cb: ({ url }) => insertImage({ e, url }),
+            cb: ({ url }) => {
+              const img = document.querySelectorAll(
+                `[src="${originalImageUrl}"]`
+              );
+
+              progress.parentElement.removeChild(progress);
+
+              if (imgElement) {
+                imgElement.style.display = "";
+                imgElement.src = url;
+              }
+            }
           });
-        }
-      }
+        });
+      }, Promise.resolve());
     });
   }
-  
+
   shouldComponentUpdate() {
     return false;
   }
-  
+
   render() {
     const { html, name } = this.props;
     return (
       <textarea
         name={name}
         className={styles.container}
-        ref={node => this._container = node}
+        ref={node => (this._container = node)}
       >
         {html}
       </textarea>
