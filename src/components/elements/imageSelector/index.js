fd09fafbeb0c4e0934cb493f59ee7a3f4355680e
Seva Zaikov <seva.zaikov@gmail.com>
2019-02-02 19:53:54 -0800

    add support for existing pictures in image selector

--- src/components/elements/imageSelector/index.js
+++ src/components/elements/imageSelector/index.js
@@ -4,12 +4,21 @@ import { actions } from "../../../store";
 import Input from "../input";
 import { readFile } from "../../../utils/files";
 import { resizeInCanvas } from "../../../utils/resize";
+import Modal from "react-modal";
+import Pictures from "../../ui/pictures";
+import { withRouter } from "react-router";
+import { parse } from "query-string";
+
+import styles from "./style.sass";
 
 const mapDispatchToProps = {
   uploadPicture: actions.api.uploadPicture
 };
 
 class ImageSelector extends React.Component {
+  state = {
+    isModalOpen: false
+  };
   onChange = async e => {
     const { name, uploadPicture } = this.props;
     const imageInput = this.container.querySelector(`input[name="${name}"]`);
@@ -32,33 +41,93 @@ class ImageSelector extends React.Component {
       });
     }
   };
+  renderModal() {
+    const {
+      name,
+      onChange,
+      location: { search }
+    } = this.props;
+    const { isModalOpen } = this.state;
+    const self = this;
+
+    function PictureComponent({ picture }) {
+      return (
+        <div
+          onClick={() => {
+            if (onChange) {
+              onChange(picture.big_url);
+            } else {
+              const imageInput = self.container.querySelector(
+                `input[name="${name}"]`
+              );
+              imageInput.value = picture.big_url;
+            }
+
+            self.setState({ isModalOpen: false });
+          }}
+        >
+          <img src={picture.small_url} style={{ height: "150px" }} />
+        </div>
+      );
+    }
+    return (
+      <Modal
+        isOpen={isModalOpen}
+        style={{ overlay: { zIndex: 11 } }}
+        onRequestClose={() => this.setState({ isModalOpen: false })}
+        contentLabel="Example Modal"
+      >
+        <Pictures params={parse(search)} PictureComponent={PictureComponent} />
+      </Modal>
+    );
+  }
   renderHint() {
     return (
       <div>
+        <span
+          className={styles.chooseLink}
+          onClick={() => this.setState({ isModalOpen: true })}
+        >
+          choose from pictures
+        </span>
         {
-          "please, paste link to the 1200px version from the website or pick a file "
+          " or paste link to the 1200px version from the website or pick a file "
         }
-        <input accept=".png,.jpg" type="file" onChange={this.onChange} />
+        <input
+          className={styles.chooseFile}
+          accept=".png,.jpg"
+          type="file"
+          onChange={this.onChange}
+        />
       </div>
     );
   }
   render() {
-    const { label, name, defaultValue } = this.props;
+    const { label, name, defaultValue, onChange, value } = this.props;
     return (
       <div ref={node => (this.container = node)}>
+        {this.renderModal()}
         <Input
+          onChange={e => {
+            if (onChange) {
+              onChange(e.target.value);
+            }
+          }}
           label={label}
           name={name}
           type={"text"}
           hint={this.renderHint()}
           defaultValue={defaultValue}
+          value={value}
         />
       </div>
     );
   }
 }
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(ImageSelector);
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(ImageSelector)
+);
